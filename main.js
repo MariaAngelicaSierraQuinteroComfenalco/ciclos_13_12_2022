2f452559757f0563be8e36bdc3b77e39ea71b119
MariaAngelicaSierraQuinteroComfenalco <angelicasierr904@gmail.com>
2022-12-19 17:40:40 -0500

    For

---
+++ main.js
@@ -0,0 +1,19 @@
+addEventListener(`DOMContentLoaded`, (e)=>{
+
+    // FOR
+    // es un bucle que consiste en tres expresiones dentro de parentesis y separadas
+    //for ([expresion-inicial]; [condicion]; [expresion-final])sentencia
+    // esa seria el ejmplo con el cual podemos crear un for
+    //En la exprecion inicial esta se utliza como una variable contador, al final el resultado es descartado
+    // La condicion evaluada para saber si se ejecuta o no 
+    //En la expresion final se usa para incrementar o cambiar el numero del contador 
+    //la sentencia se ejecuta mientras la condicion evaluada sea verdadera
+    //ejemplo:
+
+    for (var i = 0; i < 10; i++) {//se inicializa la variable con var
+        n += i;
+        mifuncion(n);
+      }
+
+
+})
